7d5c27e2e68e9869f3a9a9b7527a3db47f95da8f
Maiara Will <maiaracwill@gmail.com>
2020-05-02 02:27:40 -0300

    aula3

--- aula3/src/App.js
+++ aula3/src/App.js
@@ -1,38 +1,89 @@
-import React from 'react';
-import { Jumbotron, Button, Form, Col, Spinner } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Jumbotron, Button, Form, Col, Spinner, Alert, Modal} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import ListarMoedas from './listar-moedas';
+import './App.css';
 
 function Conversor() {
+
+  const [valor, setValor] = useState('1');
+  const [moedaDe, setMoedaDe] = useState('BRL');
+  const [moedaPara, setMoedaPara] = useState('USD');
+  const [exibirSpinner, setExibirSpinner] = useState(false);
+
+
+  function handleValor(event) {
+    setValor(event.target.value.replace(/\D/g, ''));
+  };
+
+  function handleMoedaDe(event) {
+    setMoedaDe(event.target.value);
+  };
+
+  function handleMoedaPara(event) {
+    setMoedaPara(event.target.value);
+  };
+
   return (
     <div>
+    <h1>Conversor de Moedas</h1>
+    <Alert variant="danger" show={false}>
+      Erro obtendo dados de conversão, tente novamente
+    </Alert>
     <Jumbotron>
        <Form>
          <Form.Row>
            <Col sm='3'>
             <Form.Control 
               placeholder="0"
-              value={1}
+              value={valor}
+              onChange={handleValor}
               required/>
            </Col>
            <Col sm='3'>
-            <Form.Control as="select">
+            <Form.Control as="select"
+              value={moedaDe}
+              onChange={handleMoedaDe}>
+              <ListarMoedas />
             </Form.Control>
            </Col>
            <Col sm='1' className="text-center" style={{paddingTop: "5px"}}>
             <FontAwesomeIcon icon={faAngleDoubleRight}/>
            </Col>
            <Col sm='3'>
-            <Form.Control as="select">
+            <Form.Control as="select"
+            value={moedaPara}
+            onChange={handleMoedaPara}>
+            <ListarMoedas />
             </Form.Control>
            </Col>
             <Button variant="success" type="submitt">
+              <spam className={exibirSpinner ? null : 'hidden'}> 
+              <Spinner animation="border" size="sm" />
+              </spam>
+              <spam className={exibirSpinner ? 'hidden' : null}>
               Converter
+              </spam>
             </Button>
            <Col sm='2'>
            </Col>
          </Form.Row>
        </Form>
+
+       <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Conversão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Resultado da conversão aqui...
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">
+              Nova conversão
+            </Button>
+          </Modal.Footer>
+       </Modal>
     </Jumbotron>
     </div>
   );
